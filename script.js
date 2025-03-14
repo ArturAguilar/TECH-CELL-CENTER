b7bde6abc111901f80f0852d692b09d1b1fd5de4
artur <arturaguilar426@gmail.com>
2025-03-14 00:54:58 -0300

    ATUALIZAÇÃO

--- script.js
+++ script.js
@@ -20,48 +20,38 @@ function fecharMenu() {
 // ===================================== Main =====================================
 // ===================================== Carrossel =====================================
 document.addEventListener("DOMContentLoaded", function () {
-  const slides = document.querySelectorAll(".carousel-slide");
-  const dotsContainer = document.querySelector(".carousel-dots");
-  const prevBtn = document.querySelector(".prev-btn");
-  const nextBtn = document.querySelector(".next-btn");
-
-  let currentIndex = 0;
-
-  function atualizarCarrossel() {
-      slides.forEach((slide, index) => {
-          slide.classList.toggle("active", index === currentIndex);
-      });
-
-      document.querySelectorAll(".carousel-dots span").forEach((dot, index) => {
-          dot.classList.toggle("active-dot", index === currentIndex);
-      });
-  }
-
-  function criarIndicadores() {
-      slides.forEach((_, index) => {
-          const dot = document.createElement("span");
-          dot.addEventListener("click", () => {
-              currentIndex = index;
-              atualizarCarrossel();
-          });
-          dotsContainer.appendChild(dot);
-      });
-  }
-
-  function proximoSlide() {
-      currentIndex = (currentIndex + 1) % slides.length;
-      atualizarCarrossel();
-  }
-
-  function slideAnterior() {
-      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-      atualizarCarrossel();
-  }
-
-  prevBtn.addEventListener("click", slideAnterior);
-  nextBtn.addEventListener("click", proximoSlide);
-
-  criarIndicadores();
-  atualizarCarrossel();
-  setInterval(proximoSlide, 3000); // Troca automática a cada s
-});
+    const slides = document.querySelectorAll(".carousel-slide");
+    const dotsContainer = document.querySelector(".carousel-dots");
+  
+    let currentIndex = 0;
+  
+    function atualizarCarrossel() {
+        slides.forEach((slide, index) => {
+            slide.classList.toggle("active", index === currentIndex);
+        });
+  
+        document.querySelectorAll(".carousel-dots span").forEach((dot, index) => {
+            dot.classList.toggle("active-dot", index === currentIndex);
+        });
+    }
+  
+    function criarIndicadores() {
+        slides.forEach((_, index) => {
+            const dot = document.createElement("span");
+            dot.addEventListener("click", () => {
+                currentIndex = index;
+                atualizarCarrossel();
+            });
+            dotsContainer.appendChild(dot);
+        });
+    }
+  
+    function proximoSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        atualizarCarrossel();
+    }
+  
+    criarIndicadores();
+    atualizarCarrossel();
+    setInterval(proximoSlide, 3000); // Troca automática a cada 3s
+  });  
