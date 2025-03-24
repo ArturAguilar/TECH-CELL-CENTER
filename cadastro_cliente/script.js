d9099a32d68f61e45054c337fb1c55362753787c
artur <arturaguilar426@gmail.com>
2025-03-23 22:52:37 -0300

    Adicionando boas práticas nos JS

--- cadastro_cliente/script.js
+++ cadastro_cliente/script.js
@@ -6,49 +6,42 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Função para configurar os ouvintes de eventos
 function configurarOuvintesDeEventos() {
-    // Mostra o formulário de registro de cliente e esconde a tabela de clientes
     document.getElementById('mostrarFormularioBotao').addEventListener('click', function() {
-        document.getElementById('secaoFormularioCliente').style.display = 'block';
-        document.querySelector('.tabela-clientes').style.display = 'none';
-        document.getElementById('mostrarFormularioBotao').style.display = 'none';
+        toggleFormulario(true);
     });
 
-    // Remova qualquer evento de submissão existente antes de adicionar um novo
     const formularioCliente = document.getElementById('formularioCliente');
-    formularioCliente.removeEventListener('submit', lidarComSubmissaoFormulario); // Remove o evento de submissão existente para evitar a duplicação de eventos
-    formularioCliente.addEventListener('submit', lidarComSubmissaoFormulario); // Adiciona um novo evento de submissão para lidar com a submissão do formulário
+    formularioCliente.removeEventListener('submit', lidarComSubmissaoFormulario);
+    formularioCliente.addEventListener('submit', lidarComSubmissaoFormulario);
 
-    // Adiciona um evento para filtrar os clientes com base na entrada de pesquisa
-    document.getElementById('entradaPesquisa').addEventListener('input', function() {
-        const termoPesquisa = this.value.toLowerCase(); // Obtém o termo de pesquisa digitado pelo usuário em letras minúsculas para facilitar a comparação de string
-        const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
-        const corpoTabela = document.getElementById('corpoTabelaClientes');
-        corpoTabela.innerHTML = '';
+    document.getElementById('entradaPesquisa').addEventListener('input', filtrarClientes);
+}
 
-        clientes.forEach(cliente => { // Percorre cada cliente armazenado no localStorage
-            if (cliente.nome.toLowerCase().includes(termoPesquisa)) { // Verifica se o nome do cliente inclui o termo de pesquisa digitado
-                const linha = document.createElement('tr');
-                linha.innerHTML = `
-                    <td>${cliente.id}</td>
-                    <td>${cliente.nome}</td>
-                    <td>${cliente.cpfCnpj}</td>
-                    <td>${cliente.telefone}</td>
-                    <td>${cliente.endereco}</td>
-                    <td class="acoes">
-                        <button class="editar" onclick="editarCliente(${cliente.id})">Editar</button>
-                        <button class="deletar" onclick="deletarCliente(${cliente.id})">Deletar</button>
-                    </td>
-                `;
-                corpoTabela.appendChild(linha);
-            }
-        });
+// Função para alternar a exibição do formulário
+function toggleFormulario(mostrar) {
+    document.getElementById('secaoFormularioCliente').style.display = mostrar ? 'block' : 'none'; 
+    document.querySelector('.tabela-clientes').style.display = mostrar ? 'none' : 'block';
+    document.getElementById('mostrarFormularioBotao').style.display = mostrar ? 'none' : 'block';
+}
+
+// Função para filtrar os clientes
+function filtrarClientes() {
+    const termoPesquisa = this.value.toLowerCase();
+    const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
+    const corpoTabela = document.getElementById('corpoTabelaClientes');
+    corpoTabela.innerHTML = '';
+
+    clientes.forEach(cliente => {
+        if (cliente.nome.toLowerCase().includes(termoPesquisa)) {
+            corpoTabela.appendChild(criarLinhaTabela(cliente));
+        }
     });
 }
 
 // Função para lidar com a submissão do formulário
 function lidarComSubmissaoFormulario(event) {
-    event.preventDefault(); // Previne a ação padrão do formulário (enviar a página)
-    salvarCliente(); // Chama a função para salvar o cliente
+    event.preventDefault();
+    salvarCliente();
 }
 
 // Função para salvar o cliente no localStorage
@@ -63,87 +56,96 @@ function salvarCliente() {
     const numero = document.getElementById('numero').value;
 
     const endereco = `${rua}, ${numero}, ${bairro}, ${cidade}`;
-
     const dadosCliente = {
-        id: clienteId || new Date().getTime(), // Se o clienteId não for fornecido, gera um novo id baseado no timestamp atual para o novo cliente
-        nome: nome,
-        cpfCnpj: cpfCnpj,
-        telefone: telefone,
-        endereco: endereco
+        id: gerarIdClienteUnico(),
+        nome,
+        cpfCnpj,
+        telefone,
+        endereco
     };
 
     let clientes = JSON.parse(localStorage.getItem('clientes')) || [];
 
-    if (clienteId) { // Se o clienteId for fornecido, atualiza o cliente existente com os novos dados do cliente
-        const index = clientes.findIndex(cliente => cliente.id == clienteId); // Encontra o índice do cliente existente no array de clientes com base no clienteId
-        clientes[index] = dadosCliente; // Substitui o cliente existente com os novos dados do cliente no array de clientes
+    if (clienteId) {
+        const index = clientes.findIndex(cliente => cliente.id == clienteId);
+        clientes[index] = dadosCliente;
     } else {
-        clientes.push(dadosCliente); // Adiciona o novo cliente ao array de clientes se o clienteId não for fornecido (ou seja, é um novo cliente)
+        clientes.push(dadosCliente);
     }
 
-    localStorage.setItem('clientes', JSON.stringify(clientes)); // Salva o array de clientes atualizado no localStorage para persistência dos dados entre as sessões do navegador
+    localStorage.setItem('clientes', JSON.stringify(clientes));
 
     alert('Cliente salvo com sucesso!');
-
-    document.getElementById('formularioCliente').reset(); // Limpa o formulário após salvar o cliente
-    document.getElementById('secaoFormularioCliente').style.display = 'none'; // Esconde o formulário de registro de cliente após salvar o cliente
-    document.querySelector('.tabela-clientes').style.display = 'block'; // Mostra a tabela de clientes após salvar o cliente para visualizar os clientes salvos
-    document.getElementById('mostrarFormularioBotao').style.display = 'block'; // Mostra o botão para mostrar o formulário de registro de cliente após salvar o cliente para adicionar um novo cliente
-
-    carregarClientes(); // Carrega os clientes salvos no localStorage para atualizar a tabela de clientes
+    document.getElementById('formularioCliente').reset();
+    toggleFormulario(false);
+    carregarClientes();
 }
 
-// Função para carregar os clientes do localStorage e preencher a tabela de clientes
+function gerarIdClienteUnico() {
+    let clientes = JSON.parse(localStorage.getItem('clientes')) || [];
+    let novoId;
+
+    do {
+        novoId = Math.floor(10000000000 + Math.random() * 90000000000).toString(); // Gera número de 11 dígitos
+    } while (clientes.some(cliente => cliente.id === novoId)); // Garante que o ID seja único
+
+    return novoId;
+}
+
+// Função para carregar os clientes
 function carregarClientes() {
     const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
     const corpoTabela = document.getElementById('corpoTabelaClientes');
     corpoTabela.innerHTML = '';
 
-    clientes.forEach(cliente => { // Itera sobre a lista de clientes e adiciona cada cliente como uma nova linha na tabela de clientes
-        const linha = document.createElement('tr');
-        linha.innerHTML = `
-            <td>${cliente.id}</td>
-            <td>${cliente.nome}</td>
-            <td>${cliente.cpfCnpj}</td>
-            <td>${cliente.telefone}</td>
-            <td>${cliente.endereco}</td>
-            <td class="acoes">
-                <button class="editar" onclick="editarCliente(${cliente.id})">Editar</button>
-                <button class="deletar" onclick="deletarCliente(${cliente.id})">Deletar</button>
-            </td>
-        `;
-        corpoTabela.appendChild(linha);
-    });
+    clientes.forEach(cliente => corpoTabela.appendChild(criarLinhaTabela(cliente)));
+}
+
+// Função para criar uma linha na tabela
+function criarLinhaTabela(cliente) {
+    const linha = document.createElement('tr');
+    linha.innerHTML = `
+        <td>${cliente.id}</td>
+        <td>${cliente.nome}</td>
+        <td>${cliente.cpfCnpj}</td>
+        <td>${cliente.telefone}</td>
+        <td>${cliente.endereco}</td>
+        <td class="acoes">
+            <button class="editar" onclick="editarCliente('${cliente.id}')">Editar</button>
+            <button class="deletar" onclick="deletarCliente('${cliente.id}')">Deletar</button>
+        </td>
+    `;
+    return linha;
 }
 
 // Função para editar um cliente
 function editarCliente(id) {
     const clientes = JSON.parse(localStorage.getItem('clientes')) || [];
-    const cliente = clientes.find(cliente => cliente.id == id); // Encontra o cliente a ser editado no array de clientes com base no clienteId fornecido
+    const cliente = clientes.find(cliente => cliente.id == id);
+
+    if (!cliente) return;
 
     document.getElementById('clienteId').value = cliente.id;
     document.getElementById('nome').value = cliente.nome;
     document.getElementById('cpfCnpj').value = cliente.cpfCnpj;
     document.getElementById('telefone').value = cliente.telefone;
 
-    const partesEndereco = cliente.endereco.split(', '); // Divide o endereço do cliente em partes separadas (rua, número, bairro, cidade) com base na vírgula e espaço em branco como separador
-    document.getElementById('rua').value = partesEndereco[0];
-    document.getElementById('numero').value = partesEndereco[1];
-    document.getElementById('bairro').value = partesEndereco[2];
-    document.getElementById('cidade').value = partesEndereco[3];
+    const [rua, numero, bairro, cidade] = cliente.endereco.split(', ');
+    document.getElementById('rua').value = rua;
+    document.getElementById('numero').value = numero;
+    document.getElementById('bairro').value = bairro;
+    document.getElementById('cidade').value = cidade;
 
-    document.getElementById('secaoFormularioCliente').style.display = 'block';
-    document.querySelector('.tabela-clientes').style.display = 'none';
-    document.getElementById('mostrarFormularioBotao').style.display = 'none';
+    toggleFormulario(true);
 }
 
 // Função para deletar um cliente
 function deletarCliente(id) {
     let clientes = JSON.parse(localStorage.getItem('clientes')) || [];
     if (confirm('Você tem certeza que deseja excluir esse cliente?')) {
-        clientes = clientes.filter(cliente => cliente.id != id); // Filtra os clientes para excluir o cliente com o id fornecido da lista de clientes
+        clientes = clientes.filter(cliente => cliente.id !== id);
         localStorage.setItem('clientes', JSON.stringify(clientes));
-        alert('Cliente Deletado com Sucesso!');
+        alert('Cliente deletado com sucesso!');
         carregarClientes();
     }
 }
